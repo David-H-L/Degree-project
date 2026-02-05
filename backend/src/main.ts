e6ee1aa63551084161651142ec376e65aaf2ac72
David Huanca Ledezma <davidhuancaledezma@gmail.com>
2026-02-05 18:47:40 -0400

    feat: add the last dependencies

---
+++ backend/src/main.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+//import router from './config/server.routes';
+//import { ENV } from './config/env.config';
+//import { sequelize } from './config/database.config';
+//import './modules/vote/models/vote.model'; // fix solocarga los modelos con esto!
+
+
+async function start(){
+    try {
+//        await sequelize.authenticate();
+//        await sequelize.sync();
+//        console.info("base de datos conectada");
+
+//        const PORT = ENV.PORT;
+        const PORT = 3000;
+
+        const app = express();
+
+        app.use(bodyParser.json());
+
+//        app.use('/', router)
+        // Ruta básica delete this after testing
+        app.get('/', (req, res) => {
+            res.send('Server is running');
+        });
+
+        app.listen(PORT, () => {
+            console.log("Server is running on port " + PORT);
+        });
+    } catch (error) {
+        console.error("Error in server:", error);
+    }
+
+}
+
+start();
